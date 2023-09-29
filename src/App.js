702da2790c9b1f4eb38a4391f6f98fa1bef9217a
Murad Aghazada <muradaghazada@Murads-MacBook-Pro.local>
2023-09-29 11:39:37 +0400

    Partners done and Header index done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import Container from "../src/Components/Container";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Partners from "./Components/Partners";
 
 const App = () => {
   return (
     <Container>
       <Header />
       <Hero />
+      <Partners />
     </Container>
   );
 };
